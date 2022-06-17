0a2172b24aafed65b90c1aee184d3ba4b6b0c28d
baejunwhyung <101807085+baejunwhyung@users.noreply.github.com>
2022-06-17 21:53:18 +0900

    lombok과 리팩터링

--- firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
+++ firstproject/src/main/java/com/example/firstproject/controller/ArticleController.java
@@ -4,12 +4,14 @@ package com.example.firstproject.controller;
 import com.example.firstproject.Repository.ArticleRepository;
 import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
+@Slf4j
 public class ArticleController {
     @Autowired
     private ArticleRepository articleRepository;
@@ -21,16 +23,19 @@ public class ArticleController {
 
     @PostMapping("/articles/create")
     public String createArticle(ArticleForm form){
-        System.out.println(form.toString());
+        log.info(form.toString());
+       // System.out.println(form.toString());
 
         //1. Dto를 변환 > Entity로!
         Article article=form.toEntity();
-        System.out.println(article.toString());
+        log.info(article.toString());
+       // System.out.println(article.toString());
 
 
         //2. Repository에게 Entity를 DB안에 저장하게 한다
         Article saved= articleRepository.save(article);
-        System.out.println(saved.toString());
+        log.info(saved.toString());
+        //System.out.println(saved.toString());
         return "";
     }
 }
--- firstproject/src/main/java/com/example/firstproject/entity/Article.java
+++ firstproject/src/main/java/com/example/firstproject/entity/Article.java
@@ -1,11 +1,16 @@
 package com.example.firstproject.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity //DB가 해당 객체 인식 가능
+@ToString
+@AllArgsConstructor
 public class Article {
 
     @Id  //대표값을 지정   주민등록번호처럼
@@ -16,18 +21,4 @@ public class Article {
     @Column
     private String content;
 
-    public Article(Long ID, String title, String content) {
-        this.ID = ID;
-        this.title = title;
-        this.content = content;
-    }
-
-    @Override
-    public String toString() {
-        return "Article{" +
-                "ID=" + ID +
-                ", title='" + title + '\'' +
-                ", content='" + content + '\'' +
-                '}';
-    }
 }
